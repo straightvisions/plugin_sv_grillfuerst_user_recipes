c2552bfa251b3e6ab247541d9d974c5beb55a4b6
Dennis Heiden <dennis-heiden@straightvisions.com>
2022-11-08 14:00:19 +0100

    GUR-17 - add revisions data objects

---
+++ src/Middleware/Recipes/Data/Revision_Finder_Result.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace SV_Grillfuerst_User_Recipes\Middleware\Recipes\Data;
+
+final class Revision_Finder_Result {
+    /**
+     * @var Revision_Finder_Item[]
+     */
+    public array $items = [];
+}
---
+++ src/Middleware/Recipes/Data/Revision_Reader_Item.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace SV_Grillfuerst_User_Recipes\Middleware\Recipes\Data;
+
+class Revisions_Reader_Item {
+    public ?string $id = null;
+    public ?string $recipe_id = null;
+    public ?string $data = '{}';
+    public ?string $created = '1970-01-01 00:00:00';
+}
