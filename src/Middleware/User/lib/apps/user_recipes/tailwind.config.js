ddb3c47b0e9819565ac2910e031aeba01bac041b
Matthias Bathke <matthias-bathke@straightvisions.com>
2022-11-10 17:47:55 +0100

    add webfonts (not finished yet)

--- src/Middleware/User/lib/apps/user_recipes/tailwind.config.js
+++ src/Middleware/User/lib/apps/user_recipes/tailwind.config.js
@@ -5,7 +5,11 @@ module.exports = {
     './src/**/*.{html,js}',
   ],
   theme: {
-  
+    fontFamily: {
+      sans: [
+        "Gentona Book, sans-serif",
+      ],
+    },
   },
   plugins: [
     require('@tailwindcss/forms'),
