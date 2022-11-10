305c556d392dbda1c3389a12460f3c8dfec6f096
Dennis Heiden <dennis-heiden@straightvisions.com>
2022-11-10 14:45:13 +0100

    update vendors for php8 support

--- src/vendor/composer/installed.php
+++ src/vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => '__root__',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => '7effad283d0028d871863f6e0fd95d7db6523046',
+        'reference' => 'e0d57f0bf5762183c8aaba42f6e4504a85503e6a',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         '__root__' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => '7effad283d0028d871863f6e0fd95d7db6523046',
+            'reference' => 'e0d57f0bf5762183c8aaba42f6e4504a85503e6a',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
