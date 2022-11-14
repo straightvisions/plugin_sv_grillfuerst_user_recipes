e17633c4b5032f815717bfcd810f7fe80969efa3
Dennis Heiden <dennis-heiden@straightvisions.com>
2022-11-14 16:16:15 +0100

    add more fields to recipe model js

--- src/Middleware/User/lib/apps/user_recipes/src/models/recipe/index.js
+++ src/Middleware/User/lib/apps/user_recipes/src/models/recipe/index.js
@@ -2,11 +2,17 @@ export default {
 	"user_id": 1, // @todo replace this later
 	"title": "",
 	"excerpt": "",
-	"categories": [],
-	"portions": 0,
+	"categories": [], // obsolete?
+	"portions": 0, // obsolete?
 	"featured_image": {
 		"url": null
 	},
+	"menu_type":0,
+	"kitchen_style":0,
+	"difficulty":"easy",
+	"preparation_time":0,
+	"cooking_time":0,
+	"waiting_time":0,
 	"ingredients": [
 		{
 			"id": 4823,
@@ -26,6 +32,13 @@ export default {
 			"amount": 2,
 			"unit": "",
 		}
-	]
+	],
+	"steps":[
+		{
+			"image":"https://www.grillfuerst.de/magazin/wp-content/uploads/2022/11/Blumenkohl-blanchieren.jpg",
+			"description":""
+		}
+	],
+	"newsletter": false
 	// add more here
 }
