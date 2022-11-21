946b9c8820d3e96d0620c5e15a0c0f6802e7dcb5
Dennis Heiden <dennis-heiden@straightvisions.com>
2022-11-21 09:29:20 +0100

    GUR-29 - remove common default value attribute

--- src/Middleware/User/lib/apps/user_recipes/src/components/form_common/index.js
+++ src/Middleware/User/lib/apps/user_recipes/src/components/form_common/index.js
@@ -45,7 +45,6 @@ export default function Common(props) {
 									rows={3}
 									className="block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
 									placeholder="Die leckersten Grillspieße..."
-									defaultValue={''}
 									value={formState.excerpt}
 								/>
 							</div>
