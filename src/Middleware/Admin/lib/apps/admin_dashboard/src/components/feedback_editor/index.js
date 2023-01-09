f29c4833f9ad54363276e4513e4ac6a449658ab8
Dennis Heiden <dennis-heiden@straightvisions.com>
2023-01-09 15:13:53 +0100

    GUR-30 - add release button + optimise mobile review screen

--- src/Middleware/Admin/lib/apps/admin_dashboard/src/components/feedback_editor/index.js
+++ src/Middleware/Admin/lib/apps/admin_dashboard/src/components/feedback_editor/index.js
@@ -1,9 +1,7 @@
 import React, {useState} from "react";
-import Submit from '../submit';
 import WYSIWYG from '../wysiwyg';
 import Spinner from "../spinner";
 import LocalStorage from "../local_storage";
-import ActivityMap from "../activity_map";
 
 export default function FeedbackEditor(props) {
 	const {
@@ -24,24 +22,17 @@ export default function FeedbackEditor(props) {
 	const SubmitButton = saving ? <button
 				disabled
 				type="submit"
-				className="ml-3 inline-flex justify-center gap-x-3 rounded-md border border-transparent bg-neutral-400 px-4 py-2 text-sm font-medium text-white shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 inline-flex items-center"
+				className="mt-4 inline-flex justify-center gap-x-3 rounded-md border border-transparent bg-neutral-400 px-4 py-2 text-sm font-medium text-white shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 inline-flex items-center"
 			><Spinner/> Speichert..</button> : <button
 				type="submit"
-				className="ml-3 inline-flex justify-center rounded-md border border-transparent bg-orange-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-white hover:text-orange-600 hover:border-orange-600 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+				className="mt-4 inline-flex justify-center rounded-md border border-transparent bg-orange-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-white hover:text-orange-600 hover:border-orange-600 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
 				onClick={handleClick}
 			>Feedback absenden</button>;
 	
 	return (
-		<div className="space-y-12">
-			<div className="space-y-4">
-				<WYSIWYG formState={formState} onChange={val => setLocalStorage(val)} value={localStorage}/>
-				{SubmitButton}
-			</div>
-			
-			<div className="space-y-4">
-				<h3 className="text-lg font-medium leading-6 text-gray-900">Feedback Historie</h3>
-				<ActivityMap items={formState.feedback} />
-			</div>
+		<div>
+			<WYSIWYG formState={formState} onChange={val => setLocalStorage(val)} value={localStorage}/>
+			{SubmitButton}
 		</div>
 	);
 	
--- src/Middleware/Admin/lib/apps/admin_dashboard/src/components/review/index.js
+++ src/Middleware/Admin/lib/apps/admin_dashboard/src/components/review/index.js
@@ -1,13 +1,10 @@
 import React, {useEffect, useState} from "react";
+import { useParams } from "react-router-dom";
 import Spinner from '../spinner';
 import RecipeDatasheet from '../recipe_datasheet';
 import FeedbackEditor from '../feedback_editor';
 import routes from "../../models/routes";
-
-import {
-	useParams
-} from "react-router-dom";
-
+import ActivityMap from "../activity_map";
 
 export default function Review(props) {
 	const {user} = props;
@@ -73,8 +70,6 @@ export default function Review(props) {
 		});
 	};
 	
-	console.log(formState);
-	
 	if(loading){
 		return (
 			<div className="bg-white px-4 py-12 shadow sm:rounded-lg  h-full">
@@ -83,21 +78,41 @@ export default function Review(props) {
 		)
 	}
 	
+	const handleRelease = () => {
+	
+	}
+	
+	const ReleaseButton = saving ? <button
+		disabled
+		type="submit"
+		className="col-span-12 ml-3 mt-4 inline-flex justify-center gap-x-3 rounded-md border border-transparent bg-neutral-400 px-4 py-2 text-sm font-medium text-white shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 inline-flex items-center"
+	><Spinner/> Speichert..</button> : <button
+		type="submit"
+		className="col-span-12 ml-3 mt-4 inline-flex justify-center rounded-md border border-transparent bg-orange-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-white hover:text-orange-600 hover:border-orange-600 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+		onClick={handleRelease}
+	>Rezepte freigeben</button>;
+	
 	return (
 		<div className="py-6">
 			<div className="mx-auto max-w-3xl sm:px-6 lg:grid lg:max-w-7xl lg:grid-cols-12 lg:gap-8 lg:px-8">
-				<main className="lg:col-span-9 xl:col-span-6">
+				<main className="xs:col-span-12 sm:col-span-6 xl:col-span-6 space-y-4 mb-4">
 					<RecipeDatasheet data={formState} />
+					<div className="space-y-4">
+						<h3 className="text-lg font-medium leading-6 text-gray-900">Feedback Historie</h3>
+						<ActivityMap items={formState.feedback} />
+					</div>
 				</main>
-				<aside className="hidden xl:col-span-6 xl:block">
-					<div className="sticky top-6 space-y-4">
+				<aside className="xs:col-span-12 sm:col-span-6 xl:col-span-6 xs:block">
+					<div className="sticky top-6">
 						<FeedbackEditor formState={formState} onSubmit={handleSubmit} saving={saving} />
 					</div>
 				</aside>
 			</div>
+			<div className="mx-auto my-8 max-w-3xl sm:px-6 lg:grid lg:max-w-7xl lg:grid-cols-12 lg:gap-8 lg:px-8">
+				<hr className="h-px bg-gray-400 border-0 col-span-12" />
+				{ReleaseButton}
+			</div>
 		</div>
-		
-		
 	);
 
 }
