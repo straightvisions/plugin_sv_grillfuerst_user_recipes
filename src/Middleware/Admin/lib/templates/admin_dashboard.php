9f3fc19d3486369e03c149735c7d190d02df3ce8
Dennis Heiden <dennis-heiden@straightvisions.com>
2023-01-12 18:09:35 +0100

    GUR-30 - update admin dashboard build + fix build output -> root path was wrong

--- src/Middleware/Admin/lib/templates/admin_dashboard.php
+++ src/Middleware/Admin/lib/templates/admin_dashboard.php
@@ -1,4 +1,4 @@
 <script>
-	const svgf_root_path = '/nutzerrezepte';
+	const svgf_root_path = '/nutzerrezepte-admin';
 </script>
 <div id="sv-grillfuerst-user-recipes-admin-app" class="alignfull"></div>
