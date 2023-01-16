a644cddcd371055b002129bed616ad18253e42bc
Dennis Heiden <dennis-heiden@straightvisions.com>
2023-01-16 14:40:50 +0100

    GUR-59 - add register component

--- src/Middleware/User/lib/apps/login/src/components/login/index.js
+++ src/Middleware/User/lib/apps/login/src/components/login/index.js
@@ -68,7 +68,7 @@ export default function Login(props){
 						<div>
 							<button
 								type="submit"
-								className="flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+								className="flex w-full justify-center rounded-md border border-transparent bg-orange-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-orange-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
 							>
 								Anmelden
 							</button>
--- src/Middleware/User/lib/apps/login/src/components/register/index.js
+++ src/Middleware/User/lib/apps/login/src/components/register/index.js
@@ -1,9 +1,234 @@
 import React from 'react';
+import Logo from '../logo';
 
 export default function Register(props){
 	return (
-		<div>
-			Register
+		<div className="flex min-h-full flex-col justify-center py-12 sm:px-6 lg:px-8">
+			<div className="sm:mx-auto sm:w-full sm:max-w-md">
+				<Logo />
+				<h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">Nutzerrezepte</h2>
+				<p className="mt-2 text-center text-sm text-gray-600">Mit deinen Daten erstellen wir einen Account im Grillfürst-Shop damit du dort später deine Gutscheine einlösen kannst!</p>
+			</div>
+			
+			<div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+				<div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
+					<form className="space-y-6" action="#" method="POST">
+						<div>
+							<label htmlFor="email" className="block text-sm font-medium text-gray-700">
+								Email
+							</label>
+							<div className="mt-1">
+								<input
+									id="email"
+									name="email"
+									type="email"
+									autoComplete="email"
+									required
+									className="block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+								/>
+							</div>
+						</div>
+						
+						<div>
+							<label htmlFor="password" className="block text-sm font-medium text-gray-700">
+								Password
+							</label>
+							<div className="mt-1">
+								<input
+									id="password"
+									name="password"
+									type="password"
+									autoComplete="current-password"
+									required
+									className="block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+								/>
+							</div>
+						</div>
+						
+						<div>
+							<label htmlFor="email" className="block text-sm font-medium text-gray-700">
+								Anrede
+							</label>
+							<div className="mt-1">
+								<select
+									id="salutation"
+									name="salutation"
+									defaultValue="Frau"
+									required
+									className="block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+								>
+									<option value="f">Frau</option>
+									<option value="m">Herr</option>
+									<option value="c">Firma</option>
+								</select>
+							</div>
+						</div>
+						
+						<div>
+							<label htmlFor="password" className="block text-sm font-medium text-gray-700">
+								Vorname
+							</label>
+							<div className="mt-1">
+								<input
+									id="firstname"
+									name="lastname"
+									type="text"
+									autoComplete="given-name"
+									required
+									className="block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+								/>
+							</div>
+						</div>
+						
+						<div>
+							<label htmlFor="password" className="block text-sm font-medium text-gray-700">
+								Nachname
+							</label>
+							<div className="mt-1">
+								<input
+									id="lastname"
+									name="lastname"
+									type="text"
+									autoComplete="family-name"
+									required
+									className="block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+								/>
+							</div>
+						</div>
+						
+						<div>
+							<label htmlFor="password" className="block text-sm font-medium text-gray-700">
+								Straße und Hausnummer
+							</label>
+							<div className="mt-1">
+								<input
+									id="address"
+									name="address"
+									type="text"
+									autoComplete="street-address"
+									required
+									className="block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+								/>
+							</div>
+						</div>
+						
+						<div>
+							<label htmlFor="password" className="block text-sm font-medium text-gray-700">
+								Postleiztzahl
+							</label>
+							<div className="mt-1">
+								<input
+									id="zip"
+									name="zip"
+									type="text"
+									autoComplete="postal-code"
+									required
+									className="block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+								/>
+							</div>
+						</div>
+						
+						<div>
+							<label htmlFor="password" className="block text-sm font-medium text-gray-700">
+								Ort
+							</label>
+							<div className="mt-1">
+								<input
+									id="city"
+									name="city"
+									type="text"
+									autoComplete="address-level2"
+									required
+									className="block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+								/>
+							</div>
+						</div>
+						
+						<div>
+							<label htmlFor="email" className="block text-sm font-medium text-gray-700">
+								Land
+							</label>
+							<div className="mt-1">
+								<select
+									id="salutation"
+									name="salutation"
+									defaultValue="Frau"
+									required
+									className="block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+								>
+									<option value="DE">Deutschland</option>
+									<option value="AT">Österreich</option>
+								</select>
+							</div>
+						</div>
+						
+						<div>
+							<label htmlFor="password" className="block text-sm font-medium text-gray-700">
+								Telefonnummer
+							</label>
+							<div className="mt-1">
+								<input
+									id="phone"
+									name="phone"
+									type="text"
+									autoComplete="tel"
+									required
+									className="block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+								/>
+							</div>
+						</div>
+						
+						<div className="flex items-center justify-between">
+							<div className="flex items-center">
+								<input
+									id="remember-me"
+									name="remember-me"
+									type="checkbox"
+									className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500"
+								/>
+								<label htmlFor="remember-me" className="ml-2 block text-sm text-gray-900">
+									Angemeldet bleiben
+								</label>
+							</div>
+							
+							<div className="text-sm">
+								<a href="/reset" className="font-medium text-indigo-600 hover:text-indigo-500">
+									Passwort vergessen?
+								</a>
+							</div>
+						</div>
+						
+						<div>
+							<button
+								type="submit"
+								className="flex w-full justify-center rounded-md border border-transparent bg-orange-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-orange-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+							>
+								Anmelden
+							</button>
+						</div>
+					</form>
+					
+					<div className="mt-6">
+						<div className="relative">
+							<div className="absolute inset-0 flex items-center">
+								<div className="w-full border-t border-gray-300" />
+							</div>
+							<div className="relative flex justify-center text-sm">
+								<span className="bg-white px-2 text-gray-500">Oder</span>
+							</div>
+						</div>
+						
+						<div className="mt-6">
+							<button
+								type="button"
+								className="flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+							>
+								Neuen Account erstellen
+							</button>
+						</div>
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 };
