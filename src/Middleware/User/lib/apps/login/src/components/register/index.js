53fd7eb95bd5e18f1ea0fba174370d49ab07901d
Dennis Heiden <dennis-heiden@straightvisions.com>
2023-01-17 11:00:12 +0100

    GUR-59 - optimise register form

--- src/Middleware/User/lib/apps/login/src/components/register/index.js
+++ src/Middleware/User/lib/apps/login/src/components/register/index.js
@@ -10,43 +10,11 @@ export default function Register(props){
 				<p className="mt-2 text-center text-sm text-gray-600">Mit deinen Daten erstellen wir einen Account im Grillfürst-Shop damit du dort später deine Gutscheine einlösen kannst!</p>
 			</div>
 			
-			<div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+			<div className="mt-8 sm:mx-auto sm:w-full sm:max-w-xl">
 				<div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
-					<form className="space-y-6" action="#" method="POST">
-						<div>
-							<label htmlFor="email" className="block text-sm font-medium text-gray-700">
-								Email
-							</label>
-							<div className="mt-1">
-								<input
-									id="email"
-									name="email"
-									type="email"
-									autoComplete="email"
-									required
-									className="block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
-								/>
-							</div>
-						</div>
-						
-						<div>
-							<label htmlFor="password" className="block text-sm font-medium text-gray-700">
-								Password
-							</label>
-							<div className="mt-1">
-								<input
-									id="password"
-									name="password"
-									type="password"
-									autoComplete="current-password"
-									required
-									className="block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
-								/>
-							</div>
-						</div>
-						
-						<div>
-							<label htmlFor="email" className="block text-sm font-medium text-gray-700">
+					<form className="space-y-6 grid grid-cols-2 gap-x-4" action="#" method="POST">
+						<div className="sm:col-span-2">
+							<label htmlFor="salutation" className="block text-sm font-medium text-gray-700">
 								Anrede
 							</label>
 							<div className="mt-1">
@@ -64,8 +32,8 @@ export default function Register(props){
 							</div>
 						</div>
 						
-						<div>
-							<label htmlFor="password" className="block text-sm font-medium text-gray-700">
+						<div className="sm:col-span-1">
+							<label htmlFor="firstname" className="block text-sm font-medium text-gray-700">
 								Vorname
 							</label>
 							<div className="mt-1">
@@ -80,8 +48,8 @@ export default function Register(props){
 							</div>
 						</div>
 						
-						<div>
-							<label htmlFor="password" className="block text-sm font-medium text-gray-700">
+						<div className="sm:col-span-1">
+							<label htmlFor="lastname" className="block text-sm font-medium text-gray-700">
 								Nachname
 							</label>
 							<div className="mt-1">
@@ -96,8 +64,8 @@ export default function Register(props){
 							</div>
 						</div>
 						
-						<div>
-							<label htmlFor="password" className="block text-sm font-medium text-gray-700">
+						<div className="sm:col-span-2">
+							<label htmlFor="address" className="block text-sm font-medium text-gray-700">
 								Straße und Hausnummer
 							</label>
 							<div className="mt-1">
@@ -112,8 +80,8 @@ export default function Register(props){
 							</div>
 						</div>
 						
-						<div>
-							<label htmlFor="password" className="block text-sm font-medium text-gray-700">
+						<div className="sm:col-span-1">
+							<label htmlFor="zip" className="block text-sm font-medium text-gray-700">
 								Postleiztzahl
 							</label>
 							<div className="mt-1">
@@ -128,8 +96,8 @@ export default function Register(props){
 							</div>
 						</div>
 						
-						<div>
-							<label htmlFor="password" className="block text-sm font-medium text-gray-700">
+						<div className="sm:col-span-1">
+							<label htmlFor="city" className="block text-sm font-medium text-gray-700">
 								Ort
 							</label>
 							<div className="mt-1">
@@ -144,15 +112,15 @@ export default function Register(props){
 							</div>
 						</div>
 						
-						<div>
-							<label htmlFor="email" className="block text-sm font-medium text-gray-700">
+						<div className="sm:col-span-2">
+							<label htmlFor="country" className="block text-sm font-medium text-gray-700">
 								Land
 							</label>
 							<div className="mt-1">
 								<select
-									id="salutation"
-									name="salutation"
-									defaultValue="Frau"
+									id="country"
+									name="country"
+									defaultValue="DE"
 									required
 									className="block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
 								>
@@ -162,8 +130,24 @@ export default function Register(props){
 							</div>
 						</div>
 						
-						<div>
-							<label htmlFor="password" className="block text-sm font-medium text-gray-700">
+						<div className="sm:col-span-1">
+							<label htmlFor="email" className="block text-sm font-medium text-gray-700">
+								Email
+							</label>
+							<div className="mt-1">
+								<input
+									id="email"
+									name="email"
+									type="email"
+									autoComplete="email"
+									required
+									className="block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+								/>
+							</div>
+						</div>
+						
+						<div className="sm:col-span-1">
+							<label htmlFor="phone" className="block text-sm font-medium text-gray-700">
 								Telefonnummer
 							</label>
 							<div className="mt-1">
@@ -178,55 +162,68 @@ export default function Register(props){
 							</div>
 						</div>
 						
-						<div className="flex items-center justify-between">
-							<div className="flex items-center">
+						<div className="col-span-2 inset-0 flex items-center">
+							<div className="w-full border-t border-gray-300" />
+						</div>
+						
+						<div className="sm:col-span-2">
+							<label htmlFor="password" className="block text-sm font-medium text-gray-700">
+								Passwort
+							</label>
+							<div className="mt-1">
 								<input
-									id="remember-me"
-									name="remember-me"
-									type="checkbox"
-									className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500"
+									id="password"
+									name="password"
+									type="password"
+									autoComplete="current-password"
+									required
+									className="block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
 								/>
-								<label htmlFor="remember-me" className="ml-2 block text-sm text-gray-900">
-									Angemeldet bleiben
-								</label>
-							</div>
-							
-							<div className="text-sm">
-								<a href="/reset" className="font-medium text-indigo-600 hover:text-indigo-500">
-									Passwort vergessen?
-								</a>
 							</div>
 						</div>
 						
-						<div>
+						<div className="sm:col-span-2">
+							<label htmlFor="password-repeat" className="block text-sm font-medium text-gray-700">
+								Passwort wiederholen
+							</label>
+							<div className="mt-1">
+								<input
+									id="password-repeat"
+									name="password-repeat"
+									type="password"
+									required
+									className="block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+								/>
+							</div>
+						</div>
+						
+						<div className="col-span-2 inset-0 flex items-center">
+							<div className="w-full border-t border-gray-300" />
+						</div>
+						
+						<div className="flex items-center col-span-2">
+							<input
+								id="newsletter"
+								name="newsletter"
+								type="checkbox"
+								className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500"
+							/>
+							<label htmlFor="newsletter" className="ml-2 block text-sm text-gray-900">
+								Ja, ich möchte den Newsletter erhalten.
+							</label>
+						</div>
+					
+						
+						<div className="col-span-2">
 							<button
 								type="submit"
 								className="flex w-full justify-center rounded-md border border-transparent bg-orange-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-orange-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
 							>
-								Anmelden
+								Neuen Account erstellen
 							</button>
 						</div>
 					</form>
 					
-					<div className="mt-6">
-						<div className="relative">
-							<div className="absolute inset-0 flex items-center">
-								<div className="w-full border-t border-gray-300" />
-							</div>
-							<div className="relative flex justify-center text-sm">
-								<span className="bg-white px-2 text-gray-500">Oder</span>
-							</div>
-						</div>
-						
-						<div className="mt-6">
-							<button
-								type="button"
-								className="flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-							>
-								Neuen Account erstellen
-							</button>
-						</div>
-					</div>
 				</div>
 			</div>
 		</div>
