614998828c120519824a9a666bea42df6c32c457
Dennis Heiden <dennis-heiden@straightvisions.com>
2023-01-17 13:28:32 +0100

    GUR-59 - prepare test route for user <-> shop communication

--- src/Middleware/User/User_Middleware.php
+++ src/Middleware/User/User_Middleware.php
@@ -7,6 +7,7 @@ use SV_Grillfuerst_User_Recipes\Middleware\Api\Api_Middleware;
 use SV_Grillfuerst_User_Recipes\Middleware\User\Service\User_Dashboard_Frontend_Service;
 use SV_Grillfuerst_User_Recipes\Adapters\Adapter;
 
+
 final class User_Middleware implements Middleware_Interface {
     private User_Dashboard_Frontend_Service $User_Dashboard_Frontend_Service;
     private Api_Middleware $Api_Middleware;
@@ -52,6 +53,33 @@ final class User_Middleware implements Middleware_Interface {
         //@todo add switch for redirect to login template
         $this->Adapter->Asset()->add('app', 'User/lib/apps/user_recipes/dist/user_recipes.build.js');
         return $this->User_Dashboard_Frontend_Service->get($atts);
+    }
+
+    public function rest_login( $request ) {
+        $Request = $this->Adapter->Request()->set($request);
+        $params = $Request->getParams();
+        //$results = $this->Recipe_Finder_Service->get_list(null, $params);
+
+        $url = 'https://www.grillfuerst.de/index.php?page=callback&page_action=gf_magazine&call=api&endpoint=account-login';
+
+        // Initialize the cURL session
+        $ch = curl_init();
+
+// Set the URL of the server you want to call
+        curl_setopt($ch, CURLOPT_URL, $url);
+
+// Return the response as a string instead of outputting it
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+// Execute the cURL request
+        $response = curl_exec($ch);
+
+// Close the cURL session
+        curl_close($ch);
+
+        // implement wp_response adapter + services
+        $response = new \WP_REST_Response($results, 200); // @todo remove this when adapter is available
+        return $response;
     }
 
     public function test() {
