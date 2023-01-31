6f7382d803d09fc4116abd319bed084de4bca35d
Dennis Heiden <dennis-heiden@straightvisions.com>
2023-01-31 10:01:40 +0100

    GUR-59 - add register handler to register app + middleware tunnel route + fix setter handler for all forms

--- src/Middleware/User/lib/apps/login/src/components/reset/index.js
+++ src/Middleware/User/lib/apps/login/src/components/reset/index.js
@@ -88,6 +88,7 @@ export default function Reset(props){
 									autoComplete="email"
 									required
 									className="block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+									onChange={handleEmail}
 								/>
 							</div>
 						</div>
