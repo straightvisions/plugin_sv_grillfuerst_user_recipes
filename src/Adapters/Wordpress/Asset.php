bb702bd7ea614b9dae3f2a65d99682ac00a984c8
Dennis Heiden <dennis-heiden@straightvisions.com>
2023-02-27 17:01:13 +0100

    GUR-59 - add adapter script localize

--- src/Adapters/Wordpress/Asset.php
+++ src/Adapters/Wordpress/Asset.php
@@ -4,12 +4,13 @@ namespace SV_Grillfuerst_User_Recipes\Adapters\Wordpress;
 
 use function wp_enqueue_script;
 use function wp_register_script;
+use function wp_localize_script;
 use function plugin_dir_url;
 
 final class Asset {
     private $assets = [];
 
-    public function add(string $handle, string $src): void {
+    public function add(string $handle, string $src, array $localize = []): void {
         $src = plugin_dir_url('sv-grillfuerst-user-recipes/*') . 'src/Middleware/' . $src;
         // save origin
         $this->assets[$handle] = $src;
@@ -17,5 +18,11 @@ final class Asset {
         // wordpress specific
         wp_register_script( $handle, $src, [], filemtime( $src ), true);
         wp_enqueue_script( $handle, $src);
+        if($localize){
+            foreach($localize as $key => $script){
+                wp_localize_script($handle, $handle.'_'.$key, $script);
+            }
+
+        }
     }
 }
