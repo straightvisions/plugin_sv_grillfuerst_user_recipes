f8e052ed8ce93bc18fb3464f6448ea9d94bfa3e8
Dennis Heiden <dennis-heiden@straightvisions.com>
2023-03-04 15:01:43 +0100

    GUR-134 - add email templates + email middleware methods

--- src/Middleware/Email/Email_Middleware.php
+++ src/Middleware/Email/Email_Middleware.php
@@ -11,28 +11,52 @@ class Email_Middleware implements Middleware_Interface {
     private $mailer;
     private $twig;
 
+    private $data = [
+      'from'=>'',
+      'to'=>'',
+      'subject'=>'',
+      'body'=>'',
+    ];
+
     public function __construct(MailerInterface $mailer, Environment $twig) {
         $this->mailer = $mailer;
         $this->twig   = $twig;
     }
 
-    public function __invoke($request) {
+    public function send(string $type = 'default') {
+        $d = $this->data;
+
         $email = (new Email())
-            ->from('sender@example.com')
-            ->to('recipient@example.com')
-            ->subject('Example Email')
+            ->from($d['from'])
+            ->to($d['to'])
+            ->subject($d['subject'])
             ->html(
-                $this->render([
-                    'subject' => 'Example Email',
-                    'content' => 'This is an example email.'
+                $this->render($type, [
+                    'subject' => $d['subject'],
+                    'content' => $d['body']
                 ])
             );
 
         $this->mailer->send($email);
     }
 
-    private function render(array $params): string {
+    private function render(string $type, array $params): string {
         $type = 'published';
-        return $this->twig->render('template/.'.$type.'.html.twig', $params);
+        $type = $this->isTemplate($type) ? $type : 'default';
+        return  $this->twig->render('Template/'.$type.'.html.twig', $params) ;
+    }
+
+    private function isTemplate(string $type): bool{
+        return file_exists(__DIR__.'/Template/'.$type.'.html.twig');
+    }
+
+    public function set(string $field, string $value): void{
+        if(isset($this->data[$field])){
+            $this->data[$field] = $value;
+        }
+    }
+
+    public function get(string $field): string{
+        return isset($this->data[$field]) ? $this->data['field'] : '';
     }
 }
