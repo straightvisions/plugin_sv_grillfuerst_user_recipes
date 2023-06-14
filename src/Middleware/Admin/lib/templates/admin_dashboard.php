7f08fa648a4fe8970c646f37eb19c41382474e63
Dennis Heiden <dennis-heiden@straightvisions.com>
2023-06-14 12:32:55 +0200

    hotfix - root url for app (needs to be moved to config)

--- src/Middleware/Admin/lib/templates/admin_dashboard.php
+++ src/Middleware/Admin/lib/templates/admin_dashboard.php
@@ -1,4 +1,4 @@
-<script>
-	const svgf_root_path = '/community-rezepte/admin';
+<script type="text/javascript">
+	const svgf_root_path = '/magazin/community-rezepte/admin';
 </script>
 <div id="sv-grillfuerst-user-recipes-admin-app" class="alignfull"></div>
