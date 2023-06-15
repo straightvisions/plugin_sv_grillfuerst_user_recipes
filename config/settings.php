1f9c90d5e14dc06a08c653e8b985368a4e974029
Dennis Heiden <dennis-heiden@straightvisions.com>
2023-06-15 14:46:04 +0200

    fix missing defined checks to settings

--- config/settings.php
+++ config/settings.php
@@ -13,10 +13,10 @@ $settings = [
     ],
 ];
 
-$settings['db']['host'] = DB_HOST;
-$settings['db']['database'] = DB_NAME;
-$settings['db']['username'] = DB_USER;
-$settings['db']['password'] = DB_PASSWORD;
+$settings['db']['host'] = defined('DB_HOST') ? DB_HOST : 'localhost';
+$settings['db']['database'] = defined('DB_NAME') ? DB_NAME : null;
+$settings['db']['username'] = defined('DB_USER') ? DB_USER : null;
+$settings['db']['password'] = defined('DB_PASSWORD') ? DB_PASSWORD : null;
 $settings['db']['driver'] = \Cake\Database\Driver\Mysql::class;
 
 // Path settings
@@ -26,26 +26,26 @@ $settings['public'] = $settings['root'] . '/public';
 $settings['template'] = $settings['root'] . '/templates';
 
 // AUTH
-$settings['basic_auth_header'] = GF_USER_RECIPES_APP_BASIC_AUTH;
-$settings['bearer_auth_header'] = GF_USER_RECIPES_APP_BEARER_AUTH;
-$settings['wordpress_export_auth'] = GF_USER_RECIPES_WORDPRESS_EXPORT_AUTH; // format: "Basic base64String"
+$settings['basic_auth_header'] = defined('GF_USER_RECIPES_APP_BASIC_AUTH') ? GF_USER_RECIPES_APP_BASIC_AUTH : '';
+$settings['bearer_auth_header'] = defined('GF_USER_RECIPES_APP_BEARER_AUTH') ? GF_USER_RECIPES_APP_BEARER_AUTH : '';
+$settings['wordpress_export_auth'] = defined('GF_USER_RECIPES_WORDPRESS_EXPORT_AUTH') ? GF_USER_RECIPES_WORDPRESS_EXPORT_AUTH : ''; // format: "Basic base64String"
 // combine auth headers to final header
 $settings['auth_header'] = [];
 $settings['auth_header'][] = $settings['basic_auth_header'] ? 'Basic ' . $settings['basic_auth_header'] : '';
 $settings['auth_header'][] = $settings['bearer_auth_header'] ? 'Bearer ' . $settings['bearer_auth_header'] : '';
 $settings['auth_header'] = implode(',', array_filter($settings['auth_header']));
 // URLS
-$settings['wordpress_domain'] = GF_USER_RECIPES_BASE_URL;
-$settings['login_server_url'] = GF_USER_RECIPES_LOGIN_SERVER_URL;
-$settings['loggedin_server_url'] = GF_USER_RECIPES_LOGGEDIN_SERVER_URL;
-$settings['reset_server_url'] = GF_USER_RECIPES_RESET_SERVER_URL;
-$settings['register_server_url'] = GF_USER_RECIPES_REGISTER_SERVER_URL;
-$settings['customer_info_server_url'] = GF_USER_RECIPES_USER_INFO_SERVER_URL;
-$settings['voucher_create_server_url'] = GF_USER_RECIPES_VOUCHER_CREATE_SERVER_URL;
-$settings['voucher_check_server_url'] = GF_USER_RECIPES_VOUCHER_CHECK_SERVER_URL;
-$settings['get_products_server_url'] = GF_USER_RECIPES_GET_PRODUCTS_URL;
-$settings['wordpress_export_url'] = GF_USER_RECIPES_WORDPRESS_EXPORT_URL;
-$settings['wordpress_export_media_url'] = GF_USER_RECIPES_WORDPRESS_EXPORT_MEDIA_URL;
+$settings['wordpress_domain'] = defined('GF_USER_RECIPES_APP_BASIC_AUTH') ? GF_USER_RECIPES_BASE_URL : '';
+$settings['login_server_url'] = defined('GF_USER_RECIPES_LOGIN_SERVER_URL') ? GF_USER_RECIPES_LOGIN_SERVER_URL : '';
+$settings['loggedin_server_url'] = defined('GF_USER_RECIPES_LOGGEDIN_SERVER_URL') ? GF_USER_RECIPES_LOGGEDIN_SERVER_URL : '';
+$settings['reset_server_url'] = defined('GF_USER_RECIPES_RESET_SERVER_URL') ? GF_USER_RECIPES_RESET_SERVER_URL : '';
+$settings['register_server_url'] = defined('GF_USER_RECIPES_REGISTER_SERVER_URL') ? GF_USER_RECIPES_REGISTER_SERVER_URL : '';
+$settings['customer_info_server_url'] = defined('GF_USER_RECIPES_USER_INFO_SERVER_URL') ? GF_USER_RECIPES_USER_INFO_SERVER_URL : '';
+$settings['voucher_create_server_url'] = defined('GF_USER_RECIPES_VOUCHER_CREATE_SERVER_URL') ? GF_USER_RECIPES_VOUCHER_CREATE_SERVER_URL : '';
+$settings['voucher_check_server_url'] = defined('GF_USER_RECIPES_VOUCHER_CHECK_SERVER_URL') ? GF_USER_RECIPES_VOUCHER_CHECK_SERVER_URL : '';
+$settings['get_products_server_url'] = defined('GF_USER_RECIPES_GET_PRODUCTS_URL') ? GF_USER_RECIPES_GET_PRODUCTS_URL : '';
+$settings['wordpress_export_url'] = defined('GF_USER_RECIPES_WORDPRESS_EXPORT_URL') ? GF_USER_RECIPES_WORDPRESS_EXPORT_URL : '';
+$settings['wordpress_export_media_url'] = defined('GF_USER_RECIPES_WORDPRESS_EXPORT_MEDIA_URL') ? GF_USER_RECIPES_WORDPRESS_EXPORT_MEDIA_URL : '';
 
 $settings['logger'] = [
     'name' => 'app',
@@ -56,15 +56,15 @@ $settings['logger'] = [
 ];
 
 $settings['mailer'] = [
-    'mode' => GF_USER_RECIPES_MAIL_MODE,
+    'mode' => defined('GF_USER_RECIPES_MAIL_MODE') ? GF_USER_RECIPES_MAIL_MODE : 'normal',
     'smtp' => [
         'type'=> '',
-        'host'=> GF_USER_RECIPES_MAIL_SMTP_HOST,
-        'port'=> GF_USER_RECIPES_MAIL_SMTP_PORT,
-        'username'=> GF_USER_RECIPES_MAIL_SMTP_USERNAME,
-        'password'=> GF_USER_RECIPES_MAIL_SMTP_PASSWORD,
+        'host'=> defined('GF_USER_RECIPES_MAIL_SMTP_HOST') ? GF_USER_RECIPES_MAIL_SMTP_HOST : null,
+        'port'=> defined('GF_USER_RECIPES_MAIL_SMTP_PORT') ? GF_USER_RECIPES_MAIL_SMTP_PORT : null,
+        'username'=> defined('GF_USER_RECIPES_MAIL_SMTP_USERNAME') ? GF_USER_RECIPES_MAIL_SMTP_USERNAME : null,
+        'password'=> defined('GF_USER_RECIPES_MAIL_SMTP_PASSWORD') ? GF_USER_RECIPES_MAIL_SMTP_PASSWORD : null,
     ],
-    'from' => 'noreply@grillfuerst.de'
+    'from' => defined('GF_USER_RECIPES_MAIL_FROM') ? GF_USER_RECIPES_MAIL_FROM : 'rezepte@grillfuerst.de',
 ];
 
 return $settings;
