022066c7c40ab33e8aaa2f0a7096662c26ff72ce
Dennis Heiden <dennis-heiden@straightvisions.com>
2023-06-16 19:32:11 +0200

    fix admin user info cache

--- src/Middleware/Admin/lib/apps/admin_dashboard/src/modules/user/index.js
+++ src/Middleware/Admin/lib/apps/admin_dashboard/src/modules/user/index.js
@@ -16,12 +16,12 @@ const user = {
 	
 	init: () => {
 		const cacheName = "admin-info-cache";
-		const maxAge = 24 * 60 * 60 * 1000; // 24 hours in milliseconds
+		const maxAge = 60 * 60 * 1000; // 1 hour in milliseconds
 		
 		caches.open(cacheName).then((cache) => {
 			cache.match(routes.getAdminInfo).then((response) => {
 				//@todo check why the check hasOwnProperty is needed - somehow the response has no then method
-				if (response.hasOwnProperty('then') && response && Date.now() - new Date(response.headers.get("date")).getTime() < maxAge) {
+				if (response && Date.now() - new Date(response.headers.get("date")).getTime() < maxAge) {
 					// If the response is in the cache and not older than 24 hours, return it
 					return response.then(response => headers.parseResponse(response)).then(res => {
 						
@@ -58,6 +58,10 @@ const user = {
 							
 							user.initialised = true;
 							user.isLoggedIn = payload.isLoggedIn;
+						})
+						.catch((error) => {
+							// Handle the error if the response status is invalid
+							console.error(error);
 						});
 				}
 			});
--- src/Middleware/Api/Api_Middleware.php
+++ src/Middleware/Api/Api_Middleware.php
@@ -2,7 +2,6 @@
 
 namespace SV_Grillfuerst_User_Recipes\Middleware\Api;
 
-use Psr\Container\ContainerInterface;
 use SV_Grillfuerst_User_Recipes\Interfaces\Middleware_Interface;
 use SV_Grillfuerst_User_Recipes\Middleware\Api\Service\Api_Route_Service;
 use SV_Grillfuerst_User_Recipes\Middleware\Api\Service\Api_Http_Service;
@@ -16,14 +15,11 @@ final class Api_Middleware implements Middleware_Interface {
     private Api_Http_Service $Api_Http_Service;
     private Jwt_Middleware $Jwt_Middleware;
     private $Adapter;
-    private $settings;
 
     public function __construct(
         Adapter $Adapter,
-        Jwt_Middleware $Jwt_Middleware,
-        ContainerInterface $container
+        Jwt_Middleware $Jwt_Middleware
     ) {
-        $this->settings = $container->get('settings');
 
         add_action('rest_api_init', [$this, 'init']); //@todo move this to an adapter
         $this->Api_Route_Service = new Api_Route_Service();
@@ -32,14 +28,6 @@ final class Api_Middleware implements Middleware_Interface {
         $this->Jwt_Middleware = $Jwt_Middleware;
 
         \add_filter( 'rest_authentication_errors', [$this, 'allow_custom_routes']);
-
-        if(isset($this->settings['api_key_generation']) && $this->settings['api_key_generation'] === true){
-            $this->add([
-                'route' => '/api/key',
-                'args'  => ['methods' => 'GET', 'callback' => [$this, 'rest_api_key_generate'], 'permission_callback' => '__return_true']
-            ]);
-        }
-
     }
 
     public function allow_custom_routes($result){
@@ -75,39 +63,29 @@ final class Api_Middleware implements Middleware_Interface {
         //@todo refactor to align with RFC 7807 https://tools.ietf.org/html/rfc7807
         $Request = $this->Adapter->Request()->set($request);
         $JWT = $this->Jwt_Middleware;
-        $response = null;
 
-        // validate api key // disabled for the moment, maybe we need it later
-        /*if($this->validate_api_key($Request) === false){
-            $response = new \WP_REST_Response(['status'=>'error', 'message'=>'X-API-KEY invalid'], 200);
-        }*/
-
-        // validate access token
         $validateRequest    = $JWT->validateRequest($Request);
         // optional validation
         $validateRole       = isset($perms[0]) && $perms[0] !== '*' ? $JWT->isRole($perms[0]) : true;
         $validateCan        = isset($perms[1]) && $perms[0] !== '*' ? $JWT->can($perms[1]) : true;
         $validateCustom     = isset($perms[2]) ? $perms[2]($Request) : true;
 
-        if($response === null){
-            if(
-                $validateRequest
-                && $validateRole
-                && $validateCan
-                && $validateCustom
-            ){
-                $res = (array) $callback($Request);
-                $res[0] = (array) $res[0];
-                $res[0]['status'] = isset($res[0]['status']) ? $res[0]['status'] : 'success';
+        if(
+            $validateRequest
+            && $validateRole
+            && $validateCan
+            && $validateCustom
+        ){
+            $res = $callback($Request);
+            $response = new \WP_REST_Response($res[0], isset($res[1]) ? $res[1] : 200);
 
-                $response = new \WP_REST_Response($res[0], isset($res[1]) ? $res[1] : 200);
-
-            }else{
-                $response = new \WP_REST_Response(['status'=>'error', 'message'=>'Access denied'], 200);
-            }
+        }else{
+            $response = new \WP_REST_Response([], 401);
         }
 
-        $response->set_headers(array_merge($response->get_headers(), $headers));
+        if($headers){
+            $response->set_headers($headers);
+        }
 
         return $response;
     }
@@ -115,46 +93,8 @@ final class Api_Middleware implements Middleware_Interface {
     // public api route without any validation -> login route for example
     public function response_public($request, $callback){
         $Request = $this->Adapter->Request()->set($request);
-        $res = (array) $callback($Request);
-
-        $res[0]['status'] = isset($res[0]['status']) ? $res[0]['status'] : 'success';
-
+        $res = $callback($Request);
         return new \WP_REST_Response($res[0], isset($res[1]) ? $res[1] : 200);
-    }
-
-    private function validate_api_key($Request){
-        $output = false;
-        $key = $Request->getHeader('X-API-KEY');
-
-        if(isset($this->settings['api_keys']) && in_array($key, $this->settings['api_keys'])){
-            $output = true;
-        }
-
-        return $output;
-    }
-
-    // dev api route
-    /*
-     * allowed params:
-     * exp -> expiration time in seconds
-     * role -> consumer | admin
-     */
-    public function rest_api_key_generate($request) {
-        return $this->response_public($request, function ($Request) {
-            $length = 32;
-            $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-            $apiKey = '';
-
-            for ($i = 0; $i < $length; $i++) {
-                if ($i > 0 && $i % 6 === 0) {
-                    $apiKey .= '-';
-                }
-                $randomIndex = random_int(0, strlen($characters) - 1);
-                $apiKey .= $characters[$randomIndex];
-            }
-
-            return [['key'=>$apiKey, 'message'=>'Set this key in the X-API-KEY header for your app. The key needs to be added to the server config of the API itself.'], 200];
-        });
     }
 
 }
