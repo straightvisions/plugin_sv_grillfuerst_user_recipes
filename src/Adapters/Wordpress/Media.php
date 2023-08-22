7288aa290851ee1cf6ce643cb2d4848d72ccb7ba
Dennis Heiden <dennis-heiden@straightvisions.com>
2023-08-22 17:44:30 +0200

    GUR-377 - media adapter - instead of direct import, move file and then import to wp media lib

--- src/Adapters/Wordpress/Media.php
+++ src/Adapters/Wordpress/Media.php
@@ -1,55 +1,54 @@
 <?php
-
 namespace SV_Grillfuerst_User_Recipes\Adapters\Wordpress;
-
-use function wp_insert_attachment;
-use function wp_update_attachment_metadata;
-use function wp_generate_attachment_metadata;
-use function wp_update_post;
-use function update_post_meta;
 
 final class Media {
 
     public function add($file, $meta = []): int {
+    
         if ($file && $file['type'] !== 'directory') {
-            $path = $file['tmp_name'];
+            $uploadDir = wp_upload_dir();
+            $filePath = $uploadDir['path'] . '/' . $file['name'];
 
-            // Prepare image data for WordPress Media Library
-            $attachment = array(
-                'post_title' => $file['name'],
-                'post_content' => '',
-                'post_status' => 'inherit',
-                'post_mime_type' => mime_content_type($path),
-            );
+            // Move the file from the temporary location to the uploads directory
+            if (copy($file['tmp_name'], $filePath)) {
+                // Prepare image data for WordPress Media Library
+                $attachment = array(
+                    'post_title' => $file['name'],
+                    'post_content' => '',
+                    'post_status' => 'inherit',
+                    'post_mime_type' => mime_content_type($filePath),
+                );
 
-            // Insert the image into the WordPress Media Library
-            $attachmentId = wp_insert_attachment($attachment, $path);
+                // Insert the image into the WordPress Media Library
+                $attachmentId = wp_insert_attachment($attachment, $filePath);
 
-            if($attachmentId){
-                require_once(ABSPATH . 'wp-admin/includes/image.php');
-                // add image meta data
-                wp_update_attachment_metadata($attachmentId, wp_generate_attachment_metadata($attachmentId, $path));
-                // add post meta data
-                wp_update_post([
-                    'ID' => $attachmentId,
-                    'post_title' => $meta['title'] ?? '',
-                    'post_excerpt' => $meta['caption'] ?? '',
-                    'post_content' => $meta['description'] ?? '',
-                ]);
-                // add alt text
-                update_post_meta($attachmentId, '_wp_attachment_image_alt', $meta['alt_text']);
+                if ($attachmentId) {
+                    require_once(ABSPATH . 'wp-admin/includes/image.php');
+                    // Add image meta data
+                    wp_update_attachment_metadata($attachmentId, wp_generate_attachment_metadata($attachmentId, $filePath));
+                    // Add post meta data
+                    wp_update_post([
+                        'ID' => $attachmentId,
+                        'post_title' => $meta['title'] ?? '',
+                        'post_excerpt' => $meta['caption'] ?? '',
+                        'post_content' => $meta['description'] ?? '',
+                    ]);
+                    // Add alt text
+                    update_post_meta($attachmentId, '_wp_attachment_image_alt', $meta['alt_text']);
 
-            }else{
-                error_log('Error while adding media file: '.$file['name'].' to media library. -> ID is false');
+                } else {
+                    error_log('Error while adding media file: ' . $file['name'] . ' to media library. -> ID is false');
+                }
+            } else {
+                error_log('Error while moving media file: ' . $file['name'] . ' to uploads directory.');
             }
-
         }
 
         return $attachmentId ?? false;
     }
 
     public function link(int $postId, int $mediaId): int|bool{
-       // add media to post
+        // add media to post
         $result = wp_update_post([
             'ID' => $mediaId,
             'post' => $postId,
